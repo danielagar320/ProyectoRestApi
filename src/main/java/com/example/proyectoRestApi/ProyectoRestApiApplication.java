378599c7448a52e43736be71ae6f96d514abcff7
danielagar320 <daniela.garcia-r@mail.escuelaing.edu.co>
2023-08-28 01:46:50 -0500

    Primer commit

---
+++ src/main/java/com/example/proyectoRestApi/ProyectoRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectoRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyectoRestApi/ProyectoRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectoRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
